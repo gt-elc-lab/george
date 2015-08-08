cc498760cef7b2cbb8df2c333b0653c22a1304c4
SimplyFaisal <d.f.animashaun@gmail.com>
2015-08-08 15:57:36 -0400

    dev setup

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, render_template
 
-application = Flask(__name__)
-
+if not os.environ.get('PROD'):
+	application = Flask(__name__, static_url_path='')
+else:
+	application = Flask(__name__)
+	
 @application.route('/')
 def index():
     return render_template('index.html')
