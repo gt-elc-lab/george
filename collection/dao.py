e3a5d49f7467ff832dc79692267fb719eb1d3e36
Faisal Animshaun <faisal@lawn-128-61-58-175.lawn.gatech.edu>
2015-08-23 17:49:26 -0400

    added docstrings

--- collection/dao.py
+++ collection/dao.py
@@ -14,55 +14,100 @@ class MongoDao(object):
             if os.environ.get('PROD'):
                 self.db = pymongo.MongoClient()['reddit']
             else:
+     
                 self.db = pymongo.MongoClient(config.TEST_DB_URI)[config.TEST_DB_NAME]
+    @property
+    def post_collection(self):
+        """
+        The collection where the posts are stored.
+        """
+        return self.db.posts
+
+
+    @property
+    def comment_collection(self):
+        """
+        The collection where the comments are stored.
+        """
+        return self.db.comments
 
     def get_post(self, post_id):
+        """
+        Get a post model for the post with the given id.
+
+        Args:
+            post_id (str):
+
+        Returns
+            models.Post object
+        """
         if type(post_id) == unicode:
             post_id = ObjectId(post_id)
         post_record = self.db.posts.find_one({'_id': post_id})
         return models.Post.from_record(post_record)
 
+
     def get_post_comments(self, post_id):
         """
+        Get comments for a given post.
+
+        Args:
+            post_id (str):
+
+        Returns:
+            a list of models.Comment object
         """
         post = self.get_post(post_id)
         return [self.get_comment(comment_id) for comment_id in post.comments]
 
+
     def get_colleges(self):
         """
-
-        :return: List of colleges in the database
+        Returns: A list of all the colleges present in the database.
         """
         return self.db.posts.find({}).distinct('college')
+
 
     def insert_post(self, post_record):
         """
     	Inserts a post object into the database
 
-    	Inputs:
-    	    post_record: a dictionary
+    	Args:
+    	    post_record (dict):
     	
-    	Returns: an ObjectId for the post
+    	Returns: 
+            an ObjectId for the post
     	"""
         return self.db.posts.find_one_and_replace(
             {'reddit_id': post_record['reddit_id']}, post_record, projection={'_id': True},
             return_document=pymongo.collection.ReturnDocument.AFTER, upsert=True)
 
-    @property
-    def post_collection(self):
-        return self.db.posts
-
     def get_comment(self, comment_id):
+        """
+        Get a comment model.
+
+        Args:
+            comment_id (str):
+
+        Returns:
+            a models.Comment
+        """
         if type(comment_id) == unicode:
             comment_id = ObjectId(comment_id)
         comment_record = self.db.comments.find_one({'_id': comment_id})
         return models.Comment.from_record(comment_record)
 
+
     def insert_comment(self, comment_record):
+        """
+        Insert the comment into the database.
+
+        Args:
+            comment_record (dict):
+
+        Returns:
+            ObjectId for the inserted post
+        """
         return self.db.comments.find_one_and_replace(
             {'reddit_id': comment_record['reddit_id']}, comment_record, projection={'_id': True},
             return_document=pymongo.collection.ReturnDocument.AFTER, upsert=True)
-
-    @property
-    def comment_collection(self):
-        return self.db.comments
