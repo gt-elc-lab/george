c73c1f4ef110747552df733a26c30b35dd8e80ed
hjamal3 <hjamal@gatech.edu>
2015-08-23 18:06:34 -0400

    Added server endpoints

--- server.py
+++ server.py
@@ -11,3 +11,19 @@ else:
 @application.route('/')
 def index():
     return render_template('index.html')
+
+@application.route('/schools')
+def show_colleges():
+	return dao.get_colleges(self).to_json()
+
+@application.route('/post/<post_id>')	
+def show_post(post_id):
+	return dao.get_post(self, post_id).to_json()
+	
+@application.route('/comment/<comment_id>')
+def show_comment(comment_id):
+	return dao.get_comment(self, comment_id).to_json()
+	
+@application.route('/comments/<post_id>')
+def show_comments(post_id):
+	return dao.get_post_comments(self, post_id).to_json()
