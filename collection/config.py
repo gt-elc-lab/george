56a7b31bb336cb601d8c5c73db256690e24d5cef
Faisal Animshaun <faisal@lawn-128-61-58-221.lawn.gatech.edu>
2015-09-09 19:50:40 -0400

    added env in config file

--- collection/config.py
+++ collection/config.py
@@ -1,21 +1,21 @@
-
+PRODUCTION = False
 TEST_DB_URI = 'mongodb://elc:yak@ds047652.mongolab.com:47652/redditdump'
 TEST_DB_NAME = 'redditdump'
-SUBREDDITS = [{'name': 'McGill University', 'subreddit': 'mcgill'}, 
-            {'name': 'Georgia Tech', 'subreddit': 'gatech'}, 
-            {'name': 'UT Austin', 'subreddit': 'UTAustin'}, 
-            {'name': 'Penn State University', 'subreddit': 'PennStateUniversity'}, 
-            {'name': 'Purdue', 'subreddit': 'purdue'}, 
-            {'name': 'UC Berkeley', 'subreddit': 'berkeley'}, 
-            {'name': 'CalPoly Ubispo', 'subreddit': 'CalPoly'}, 
-            {'name': 'UC Santa Barbara', 'subreddit': 'ucsantabarbara'}, 
-            {'name': 'North Carolina State University', 'subreddit': 'ncsu'}, 
-            {'name': 'York University', 'subreddit': 'yorku'}, 
-            {'name': 'Texas A&M', 'subreddit': 'aggies'}, 
-            {'name': 'Arizona State University', 'subreddit': 'asu'}, 
-            {'name': 'University of Central Florida', 'subreddit': 'ucf'}, 
+SUBREDDITS = [{'name': 'McGill University', 'subreddit': 'mcgill'},
+            {'name': 'Georgia Tech', 'subreddit': 'gatech'},
+            {'name': 'UT Austin', 'subreddit': 'UTAustin'},
+            {'name': 'Penn State University', 'subreddit': 'PennStateUniversity'},
+            {'name': 'Purdue', 'subreddit': 'purdue'},
+            {'name': 'UC Berkeley', 'subreddit': 'berkeley'},
+            {'name': 'CalPoly Ubispo', 'subreddit': 'CalPoly'},
+            {'name': 'UC Santa Barbara', 'subreddit': 'ucsantabarbara'},
+            {'name': 'North Carolina State University', 'subreddit': 'ncsu'},
+            {'name': 'York University', 'subreddit': 'yorku'},
+            {'name': 'Texas A&M', 'subreddit': 'aggies'},
+            {'name': 'Arizona State University', 'subreddit': 'asu'},
+            {'name': 'University of Central Florida', 'subreddit': 'ucf'},
             {'name': 'University of British Columbia', 'subreddit': 'UBC'},
-            {'name': 'University of Maryland', 'subreddit': 'UMD'}, 
+            {'name': 'University of Maryland', 'subreddit': 'UMD'},
             {'name': 'Rochester Institute of Technology', 'subreddit': 'rit'},
             {'name': 'Ohio State University', 'subreddit': 'OSU'},
             {'name': 'University of San Diego', 'subreddit': 'ucsd'},
@@ -24,5 +24,5 @@ SUBREDDITS = [{'name': 'McGill University', 'subreddit': 'mcgill'},
 
 CREDENTIALS = ('elc-gt', 'password')
 CREDENTIALS_LIST = [ ('gt-elc', 'password'),
-                ('elc-gt', 'password'), 
+                ('elc-gt', 'password'),
                 ('elc-gt3', 'wallmoose17')]
