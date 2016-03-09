8013fdad22af21482647c36139c8d3a2dc7cd021
hjamal3 <hjamal@gatech.edu>
2016-03-08 21:54:19 -0500

    Computing Accuracy of Datasets

--- server/gthealth/pipeline/classifier.py
+++ server/gthealth/pipeline/classifier.py
@@ -7,6 +7,10 @@ class Classifier(object):
     def classify(submission):
         raise NotImplementedError()
 
+
+    def classifyAccuracy(submission):
+        raise NotImplementedError()
+    
 class SimpleClassifier(object):
 
     def classify(self, submission):
@@ -15,4 +19,18 @@ class SimpleClassifier(object):
             if word in config.keywords:
                 return True
         return False
+
+    def computeAccuracy(self, arraySamples):
+        countSamples = 0.0;
+        numLabelled = 0.0;
+        for submission in arraySamples:
+            tokens = nltk.tokenize.word_tokenize(submission.content)
+            countSamples += 1
+            for word in tokens:
+                if word in config.keywords:
+                    if submission.label:
+                        numLabelled += 1
+                    break
+        return numLabelled/countSamples        
+
 
